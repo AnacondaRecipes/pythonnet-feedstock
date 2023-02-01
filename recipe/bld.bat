f3ec3df086ba34be5c3a4c82e8cca404e683a880
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-02-01 14:22:45 -0600

    fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,13 @@
 REM Install the latest dotnet-core and SDK using dotnet-install scripts.
+REM For some reason, builders have problem deleting the `dotnet` directory that's created
+REM in this step, meaning only one build is guaranteed to succeed while others will
+REM fail because the directory exists but is unusable due to some deleted files.
+REM The solution is to create a random directory to store the dotnet SDK files.
 mkdir %BUILD_PREFIX% 2> NUL
-if exist %BUILD_PREFIX%\dotnet ( rmdir %BUILD_PREFIX%\dotnet /s /q )
 curl -L -o %BUILD_PREFIX%\dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
-powershell %BUILD_PREFIX%\dotnet-install.ps1 -InstallDir %BUILD_PREFIX%\dotnet -Version latest
-set PATH=%BUILD_PREFIX%\dotnet;%PATH%
+set suffix=%random%
+powershell %BUILD_PREFIX%\dotnet-install.ps1 -InstallDir %BUILD_PREFIX%\dotnet-%suffix% -Version latest
+set PATH=%BUILD_PREFIX%\dotnet-%suffix%;%PATH%
 
 REM Now install the package
 %PYTHON% -m pip install . --no-deps -vv
